c7d652b6c24bc817b24282b7c6b3af7a0d029039
hakim-nemar <hakim.nemar@epitech.eu>
2020-04-22 17:52:11 +0200

    send new user in bdd, documentation auth

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
@@ -31,23 +32,22 @@ class UserController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register() 
+    public function registerUser(Request $request, EntityManagerInterface $manager)
     {
         $user = new User();
 
         $form = $this->createFormBuilder($user)
-                     ->add('email', TextType::class, [ 
-                         'attr' => [ 
-                             'placeholder' => 'exemple@exemple.fr'
-                         ]
-                     ])
-                     ->add('password', PasswordType::class, [ 
-                        'attr' => [ 
-                            'placeholder' => '•••••'
-                        ]
-                    ])
+                     ->add('email')
+                     ->add('password', PasswordType::class)
                      ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+        }
+        
         return $this->render('user/register.html.twig', [
             'form' => $form->createView()
         ]);
