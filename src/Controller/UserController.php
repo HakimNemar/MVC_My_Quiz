c950775c62d2ff57add86074ef36d15778965521
hakim-nemar <hakim.nemar@epitech.eu>
2020-04-22 16:19:33 +0200

    test render login register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,5 +26,47 @@ class UserController extends AbstractController
     public function home()
     {
         return $this->render('user/home.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register() 
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+                     ->add('email', TextType::class, [ 
+                         'attr' => [ 
+                             'placeholder' => 'exemple@exemple.fr'
+                         ]
+                     ])
+                     ->add('password', PasswordType::class, [ 
+                        'attr' => [ 
+                            'placeholder' => '•••••'
+                        ]
+                    ])
+                     ->getForm();
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login() 
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+                     ->add('email')
+                     ->add('password', PasswordType::class)
+                     ->getForm();
+
+        return $this->render('user/login.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
